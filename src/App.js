947b4a1c683856c0bbe53daa1e0d79eaa94330e2
Aidan <aklane.work@gmail.com>
2023-03-27 16:22:50 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,107 @@
 import logo from './logo.svg';
 import './App.css';
 
+const data = {
+  first: "Aidan",
+  last: "Lane",
+  school: "Michigan State University",
+  graduated: false
+};
+
+const games = [
+  {
+    title: "Assassins's Creed Odyssey",
+    release: 2018
+  },
+  {
+    title: "God of War",
+    release: 2018
+  },
+  {
+    title: "Player Unknown Battle Ground",
+    release: 2017
+  }
+]
+
+const classes = [
+  {
+    title: "MI 449",
+  },
+  {
+    title: "CMSE 381",
+  },
+  {
+    title: "CSE 404",
+  },
+  {
+    title: "CSE 482",
+  },
+  {
+    title: "CSE 431",
+  }
+]
+
+function Person() {
+  return (
+    <>
+      <h1>{data.first} {data.last}</h1>
+      <h2
+        style={{
+          color: data.graduated ? "green" : "red"
+        }}
+      >{data.school}</h2>
+      
+    </>
+  );
+}
+
+function Games() {
+  return (
+    <>
+      <h2>
+        Games I have recently enjoyed
+      </h2>
+      <ul>
+        {games.map(game => 
+        <li
+         key={1}
+         style={{textAlign: "left"}}
+          
+        >
+          {game.title}: {game.release}
+        </li>)}
+      </ul>
+      
+    </>
+  );
+}
+
+function Classes() {
+  return (
+    <>
+    <h2>Spring 2023 Courses</h2>
+    <ul>
+        {classes.map(game => 
+        <li
+         key={1}
+         style={{textAlign: "left"}}
+          
+        >
+          {game.title}
+        </li>)}
+      </ul>
+    </>
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Person/>
+        <Games/>
+        <Classes/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
